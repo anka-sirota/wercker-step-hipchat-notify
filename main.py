4b9c52cbcbfc09c436794fa09f9d10c7ecff382f
railla <dev.local@gmail.com>
2014-04-01 10:32:23 +0200

    refactoring + .gitignore

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import sys
 import hipchat
 
-if len(sys.argv) < 7 :
-  print 'There should be 7 arguments'
-  sys.exit(1)
+if len(sys.argv) < 7:
+    print 'There should be 7 arguments'
+    sys.exit(1)
 
 token = str(sys.argv[1])
 room_id = str(sys.argv[2])
-from_name  = str(sys.argv[3])
-message  = str(sys.argv[4])
+message_from = str(sys.argv[3])
+message = str(sys.argv[4])
 color = str(sys.argv[5])
-
-notify = '0'
-if str(sys.argv[6]) == 'true':
-    notify = '1'
+notify = (str(sys.argv[6]) in ('true', '1', 'TRUE')) and True or False
 
 hipster = hipchat.HipChat(token=token)
-
-hipster.method('rooms/message', method='POST', parameters={'room_id': room_id, 'from': from_name, 'message': message, 'color': color, 'notify': notify})
+kargs = {
+    'room_id': room_id,
+    'message_from': message_from,
+    'message': message,
+    'color': color,
+    'notify': notify
+}
+hipster.message_room(**kargs)
